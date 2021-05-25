e8354db4d1dc156775c31d239c9f27f88f428c9a
Kenny <kannychak@qq.com>
2021-05-25 16:55:04 +0800

    Update shorten.lua

--- simple-url-shorten/shorten.lua
+++ simple-url-shorten/shorten.lua
@@ -11,4 +11,12 @@ local short_url, err = functions.url_create(long_url)
 if err then
 	functions.show_error(err)
 end
-ngx.say('{"status":1,"shorturl":"'..short_url..'"}')
+--ngx.say('{"status":1,"shorturl":"'..short_url..'"}')
+
+ngx.say('网址:'..long_url..)
+ngx.say('短网址:'..short_url)
+ngx.say('--------')
+ngx.say('ngx.var.args: '..ngx.var.args)
+ngx.say('ngx.var.uri:'..ngx.var.uri)
+ngx.say('ngx.var.request_uri:'..ngx.var.request_uri)
+
